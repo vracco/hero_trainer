a09309a96c6ac0dd0177fb32e8b94185017f1167
Vito Racco <racco.vito@gmail.com>
2013-05-09 19:56:42 +0200

    Added the hero_trainer file

---
+++ lib/hero_trainer.rb
@@ -0,0 +1,3 @@
+require File.expand_path "../hero_trainer/calendar.rb", __FILE__
+require File.expand_path "../hero_trainer/hero.rb", __FILE__
+require File.expand_path "../hero_trainer/skill.rb", __FILE__
